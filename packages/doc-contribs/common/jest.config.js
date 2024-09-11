5d3c701c0cf06c8fb10c742a35775b8a61ece376
benjaminparisel <benjamin.parisel@bonitasoft.com>
2024-09-11 17:06:51 +0200

    improve config

--- packages/doc-contribs/common/jest.config.js
+++ packages/doc-contribs/common/jest.config.js
@@ -3,4 +3,5 @@ const baseConfig = require('../jest.config.base.js');
 module.exports = {
     ...baseConfig,
     // Add any specific configurations for this package here
+
 };
--- packages/doc-contribs/jest.config.base.js
+++ packages/doc-contribs/jest.config.base.js
@@ -6,5 +6,16 @@ module.exports = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts(x)?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     clearMocks : true,
-
+    reporters: [
+        "default",
+        [ "jest-junit", {
+            outputDirectory: 'reports', // Use absolute path
+            outputName: "jest-unit.xml",
+            ancestorSeparator: " › ",
+            uniqueOutputName: "false",
+            suiteNameTemplate: "{filepath}",
+            classNameTemplate: "{classname}",
+            titleTemplate: "{title}"
+        }]
+    ]
 };
